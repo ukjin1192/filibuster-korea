fc1a40c7edadb9e8054f2729afef0a22887ed931
Uk-Jin, Yang <ukjin1192@gmail.com>
2016-03-01 04:49:27 +0900

    Edit share image for abusing page

--- filibuster/static/js/abuse.js
+++ filibuster/static/js/abuse.js
@@ -66,7 +66,7 @@ $(window).load(function() {
     container: '#kakaotalk-share',
     label: '[필리버스터 릴레이] 시민 필리버스터 페이지를 방해한 댓글들을 모았습니다',
     image: {
-      src: 'http://d1es9gk2quk02b.cloudfront.net/share-abuse.png',
+      src: 'http://d1es9gk2quk02b.cloudfront.net/share-abusing.png',
       width: '527',
       height: '326'
     },
