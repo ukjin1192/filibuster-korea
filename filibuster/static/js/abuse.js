3ddd0e5c0d354a287642586ee03a0dfda28f1710
Uk-Jin, Yang <ukjin1192@gmail.com>
2016-03-01 03:09:10 +0900

    Edit share message for abusing page

--- filibuster/static/js/abuse.js
+++ filibuster/static/js/abuse.js
@@ -64,7 +64,7 @@ $(window).load(function() {
   Kakao.init('8c5bcdda801470eb94f4db4b66f33d02');
   Kakao.Link.createTalkLinkButton({
     container: '#kakaotalk-share',
-    label: '[악플버스터 릴레이] 최근 시민 필리버스터 페이지를 교란한 악플들을 모았습니다. 앞으로 6시간! 진정한 시민들의 참여를 기다립니다.',
+    label: '[필리버스터 릴레이] 최근 시민 필리버스터 페이지를 방해한 댓플들을 모았습니다. 앞으로 6시간! 진정한 시민들의 참여를 기다립니다.',
     image: {
       src: 'http://d1es9gk2quk02b.cloudfront.net/share-new2.png',
       width: '960',
