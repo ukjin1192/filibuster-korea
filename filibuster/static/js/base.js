51cc4b47bcd68de6644dd081725ce2aa4bb98b58
Uk-Jin, Yang <ukjin1192@gmail.com>
2016-03-07 22:00:08 +0900

    Seprate JS codes into base block and custom block

---
+++ filibuster/static/js/base.js
@@ -0,0 +1,18 @@
+// Alert that line and kakaotalk messenger sharing is only available at mobile
+$(document).on('click', '#line-share, #kakaotalk-share', function() {
+  // Detect desktop browser
+  if (!('ontouchstart' in window)) {
+    alert("모바일에서만 가능합니다");
+  }
+  return false;
+});
+
+$(window).load(function() {
+  $('#loading-icon').addClass('hidden');
+
+  // Ease effect when body DOM loads
+  $('body').animate({opacity: 1}, 700);
+
+  // Attach fast-click to boost up touch reaction
+  FastClick.attach(document.body);
+});
--- filibuster/static/js/test.js
+++ filibuster/static/js/test.js
@@ -1,25 +1,8 @@
-// Alert that line and kakaotalk messenger sharing is only available at mobile
-$(document).on('click', '#line-share, #kakaotalk-share', function() {
-  // Detect desktop browser
-  if (!('ontouchstart' in window)) {
-    alert("모바일에서만 가능합니다");
-  }
-  return false;
-});
-
 $(document).on('click', '.landing-page__footer', function() {
   $.fn.fullpage.moveSectionDown();
 });
 
 $(window).load(function() {
-  $('#loading-icon').addClass('hidden');
-
-  // Ease effect when body DOM loads
-  $('body').animate({opacity: 1}, 700);
-
-  // Attach fast-click to boost up touch reaction
-  FastClick.attach(document.body);
-
 	var showNavigationMessage;
 
 	$('.fullpage-container').fullpage({
