c62d1dc5755ddd578c74f62b1ae440726f4f515e
Uk-Jin, Yang <ukjin1192@gmail.com>
2016-03-08 21:10:35 +0900

    Gather social media meta tags and share group into one block

--- filibuster/static/js/records.js
+++ filibuster/static/js/records.js
@@ -91,20 +91,4 @@ $(window).scroll(function() {
 $(window).load(function() {
   // Get initial data
   getComments('김제남', 0);
-
-  // Kakaotalk sharing
-  Kakao.init('8c5bcdda801470eb94f4db4b66f33d02');
-  Kakao.Link.createTalkLinkButton({
-    container: '#kakaotalk-share',
-    label: '[가나다] 라마바',
-    image: {
-      src: 'http://d1es9gk2quk02b.cloudfront.net/share/share-desk.jpg',
-      width: '800',
-      height: '421'
-    },
-    webButton: {
-      text: '둘러보기',
-      url: $('#kakaotalk-share__link-url').val()
-    }
-  });
 });
